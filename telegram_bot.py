b262759d050da6209ef422c2a5b69430374bbcbe
BondarevIvan <bondarevivan2001@mail.ru>
2021-01-16 00:34:13 +0300

    add sending photo

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,6 @@
 from base_reacter import BaseReacter
 from telebot import TeleBot
+import urllib
 
 
 class TelegramBot(TeleBot):
@@ -9,6 +10,18 @@ class TelegramBot(TeleBot):
         @self.message_handler(content_types=['text'])
         def send_answer(message):
             self.send_answer(message)
+
+    def send_photo(self, chat_id, path, caption):
+        with open(path, 'rb') as img:
+            self.send_photo(chat_id, img, caption=caption)
+
+    def send_photo_by_url(self, chat_id, url, caption = None):
+        if url is None:
+            return False
+        with open('temp.out', 'wb') as img:
+            img.write(urllib.request.urlopen(url).read())
+        self.send_photo(chat_id, 'temp.out', caption)
+        return True
 
     def send_answer(self, message):
         for reacter in BaseReacter.reacters:
